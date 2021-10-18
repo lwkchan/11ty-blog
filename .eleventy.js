487a36bdd4dc7ecdd38f2da4e528b8b81dc90be1
Laura Chan <chan@bryter.io>
2021-10-18 19:56:18 +0800

    removed contact

--- .eleventy.js
+++ .eleventy.js
@@ -78,7 +78,6 @@ module.exports = function(eleventyConfig) {
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("static/");
-  eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/css/inline.css");
 
   /* Markdown Plugins */
