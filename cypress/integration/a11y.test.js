d6d1f66aa703ee51d6e4797ab8e526bae320ec86
Laura Chan <laura.chan@oysterhr.com>
2023-05-13 19:09:56 +0800

    Improved color contrast for comments in codeblocks

--- cypress/integration/a11y.test.js
+++ cypress/integration/a11y.test.js
@@ -1,23 +1,29 @@
 /// <reference types="Cypress" />
 
-describe('Accessibility', () => {
-  it('homepage has no detectable a11y violations on load', () => {
-    cy.visit('/')
-    cy.injectAxe()
-    cy.checkA11y()
+function throwViolations(violations) {
+  throw violations;
+}
+
+describe("Accessibility", () => {
+  it("homepage has no detectable a11y violations on load", () => {
+    cy.visit("/");
+    cy.injectAxe();
+    cy.checkA11y();
   });
 
-  it('blogpage has no detectable a11y violations on load', () => {
-    cy.visit('/blog')
-    cy.injectAxe()
-    cy.checkA11y()
-  })
+  it("blogpage has no detectable a11y violations on load", () => {
+    cy.visit("/blog");
+    cy.injectAxe();
+    cy.checkA11y();
+  });
 
-  it('latest blogpost has no detectable a11y violations on load', () => {
-    cy.get('article').first().within(() => {
-      cy.get('a').click()
-    })
-    cy.injectAxe()
-    cy.checkA11y()
-  })
+  it("latest blogpost has no detectable a11y violations on load", () => {
+    cy.get("article")
+      .first()
+      .within(() => {
+        cy.get("a").click();
+      });
+    cy.injectAxe();
+    cy.checkA11y(null, null, throwViolations);
+  });
 });
